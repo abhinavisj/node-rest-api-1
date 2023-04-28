01fce91fab9fcb65d97c7a37006b161c4e4fc098
abhinavisj <abhinav.j@infosoftjoin.in>
2023-04-28 20:47:13 +0530

    update, delete

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -1,4 +1,4 @@
-const { create } = require('./user.service');
+const { create, getUsers, getUserByUserId, updateUser, deleteUser } = require('./user.service');
 
 const { genSaltSync, hashSync } = require("bcrypt")
 
@@ -19,6 +19,81 @@ module.exports = {
             return res.status(200).json({
                 success: 1,
                 data: results.insertId
+            })
+        })
+    },
+
+    getUserByUserId : (req, res) => {
+        const id = req.param.id;
+        getUserByUserId(id, (err, results) => {
+            if(err) {
+                console.log(err);
+                return;
+            }
+            if (!results) {
+                return res.json({
+                    success: 0,
+                    message: "Record not Found"
+                });
+            }
+            return res.json({
+                success: 1,
+                data:results
+            })
+        })
+    },
+
+    getUsers: (req, res) => {
+        getUsers( (err, results) => {
+            if(err) {
+                console.log(err);
+                return;
+            }
+            return res.json({
+                success: 1,
+                data:results
+            })
+        })
+    },
+
+    updateUser: (req, res) => {
+        const body = req.body;
+        const salt = genSaltSync(10);
+        body.password = hashSync(body.password, salt);
+        updateUser(body, (err, results) => {
+            if(err) {
+                console.log(err);
+                return
+            }
+            if(!results) {
+                return res.json({
+                    success: 0,
+                    message: "Failed to update user"
+                })
+            }
+            return res.json({
+                success: 1,
+                message: "Update successfully"
+            })
+        })
+    },
+
+    deleteUser: (req, res) => {
+        const data = req.body;
+        deleteUser(data, (err, results) => {
+            if(err) {
+                console.log(err);
+                return;
+            }
+            if(!results) {
+                return res.json({
+                    success: 0,
+                    message : "Record not found"
+                })
+            }
+            return res.json({
+                success: 1,
+                message: "User deleted successfully."
             })
         })
     }
--- api/users/user.service.js
+++ api/users/user.service.js
@@ -22,6 +22,67 @@ module.exports = {
                     return callback(null, results)
                 }
         );
+    },
+
+    getUsers: callback => {
+        pool.query(
+            `select id,firstName, lastName, gender, email, password, number from registration`,
+            [],
+            (error, results, fields) => {
+                if(error) {
+                    return callback(error);
+                }
+                return callback(null, results);
+            }
+        )
+    },
+
+    getUserByUserId: (id, callback) => {
+        pool.query(
+            `select id,firstName, lastName, gender, email, password, number from registration where id = ? `,
+            [id],
+            (error, results, fields) => {
+                if(error) {
+                    return callback(error);
+                }
+                return callback(null, results[0]);
+            }
+        )
+    },
+
+    updateUser: (data, callback) => {
+        pool.query(
+            `update registration set firstName=?, lastName=?, gender=?, email=?, password=?, number=? where id = ? `,
+            [
+                    data.firstName,
+                    data.lastName,
+                    data.gender,
+                    data.email,
+                    data.password,
+                    data.number,
+                    data.id
+            ],
+            (error, results, fields) => {
+                console.log(error)
+                if(error) {
+                    return callback(error);
+                }
+                return callback(null, results);
+            }
+        )
+    },
+
+    deleteUser: (data, callback) => {
+        pool.query(
+            `delete registration where id = ? `,
+            [data.id],
+            (error, results, fields) => {
+                if(error) {
+                    return callback(error);
+                }
+                return callback(null, results[0]);
+            }
+        )
     }
 }
 
